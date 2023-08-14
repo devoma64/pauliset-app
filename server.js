3580d7b38295af61b7d2c213d38e4812bd1d56b7
devoma64 <devoma64@gmail.com>
2023-08-14 18:22:46 +0100

    route finally fixed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use("/static", express.static(path.resolve(__dirname, "frontend", "static"))
  
 app.get("/*", (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(process.env.PORT || 5050, () => console.log("Server running"));
